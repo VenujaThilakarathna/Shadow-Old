c704c8575308e4abc725af20726cc800970bdba4
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-05-06 08:53:24 +0530

    Update zombies.py

--- DaisyX/modules/zombies.py
+++ DaisyX/modules/zombies.py
@@ -1,5 +1,5 @@
- __help__ = """
+__help__ = """
     ⦁ `/zombies`*:* Check if any deleted accounts available in the group member list and give results
     ⦁ `/zombies` clean *:* Remove deleted accounts in the group member list 
     """
-    __mod_name__ = "Zombies ⚰️"
+__mod_name__ = "Zombies ⚰️"
