4d8ef3e1b9f51a063a5e834809370f88bace1cfc
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-05-06 08:47:36 +0530

    Update muting.py

--- DaisyX/modules/muting.py
+++ DaisyX/modules/muting.py
@@ -365,19 +365,6 @@ def stemp_mute(update: Update, context: CallbackContext) -> str:
 
     return ""
 
-
-__help__ = """
-*Admins only:*
- ✪ /mute `<userhandle>`*:* silences a user. Can also be used as a reply, muting the replied to user.
- ✪ /smute `<userhandle>`*:* silences a user without notifying. Can also be used as a reply, muting the replied to user.
- ✪ /tmute `<userhandle> x(m/h/d)`*:* mutes a user for x time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
- ✪ /stmute <userhandle> x(m/h/d)`*:* mutes a user for x time without notifying. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
- ✪ /unmute `<userhandle>`*:* unmutes a user. Can also be used as a reply, muting the replied to user.
- 
- _NOTE:_
- If you set Log Channels, you will get logs of Silent mutes. Check *Logger📝* module to know more about Log Channel.
-"""
-
 MUTE_HANDLER = CommandHandler("mute", mute)
 SMUTE_HANDLER = CommandHandler("smute", smute)
 UNMUTE_HANDLER = CommandHandler("unmute", unmute)
@@ -390,7 +377,6 @@ dispatcher.add_handler(UNMUTE_HANDLER)
 dispatcher.add_handler(TEMPMUTE_HANDLER)
 dispatcher.add_handler(STEMPMUTE_HANDLER)
 
-__mod_name__ = "Muting 🔇"
 __handlers__ = [
     MUTE_HANDLER,
     SMUTE_HANDLER,
