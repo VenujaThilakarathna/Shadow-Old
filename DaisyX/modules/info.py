ef5c78cdb58cf4996f24c5d22935a671540eac97
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-05-06 07:31:11 +0530

    Update info.py

--- DaisyX/modules/info.py
+++ DaisyX/modules/info.py
@@ -534,7 +534,7 @@ __help__ = """
  • /setme `<text>`*:* will set your info
  • /me*:* will get your or another user's info.
 Examples:
- `/setme I am @DeshadeethThisarana`
+ `/setme I am a Dragon`
  `/me @username(defaults to yours if no user specified)`
 
 *Information others add on you:* 
