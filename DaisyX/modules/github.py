09ce1fbebfc82c6ad971ed6319dbeb57f5eb395c
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-05-06 08:42:15 +0530

    Create github.py

---
+++ DaisyX/modules/github.py
@@ -0,0 +1,5 @@
+__help__ = """
+✪ /git: Returns info about a GitHub user or organization.
+✪ /repo: Return the GitHub user or organization repository list (Limited at 40).
+"""
+__mod_name__ = "Github 🐱"
