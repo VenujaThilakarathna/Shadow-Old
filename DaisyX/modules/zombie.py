aadc2625318486cbbee3528062c47c65573a2bda
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-05-06 08:28:34 +0530

    Update zombie.py

--- DaisyX/modules/zombie.py
+++ DaisyX/modules/zombie.py
@@ -110,8 +110,4 @@ async def zombies(event):
 
     await cleaning_zombies.edit(del_status)
     
-    __help__ = """
-    ⦁ `/zombies`*:* Check if any deleted accounts available in the group member list and give results
-    ⦁ `/zombies` clean *:* Remove deleted accounts in the group member list 
-    """
-    __mod_name__ = "Zombies ⚰️"
+   
