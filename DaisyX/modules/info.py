f027188e939b0f2617bc5056cfa9dac389e8eb23
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-05-06 08:32:38 +0530

    Update info.py

--- DaisyX/modules/info.py
+++ DaisyX/modules/info.py
@@ -525,31 +525,6 @@ def __user_info__(user_id):
     return result
 
 
-__help__ = """
-*ID:*
- • /id*:* get the current group id. If used by replying to a message, gets that user's id.
- • /gifid*:* reply to a gif to me to tell you its file ID.
-
-*Self addded information:* 
- • /setme `<text>`*:* will set your info
- • /me*:* will get your or another user's info.
-Examples:
- `/setme I am a Dragon`
- `/me @username(defaults to yours if no user specified)`
-
-*Information others add on you:* 
- • /bio*:* will get your or another user's bio. This cannot be set by yourself.
-• /setbio `<text>`*:* while replying, will save another user's bio 
-Examples:
- `/bio @username(defaults to yours if not specified).`
- `/setbio This user is a wolf` (reply to the user)
-
-*Overall Information about you:*
- • /inf`*:* get information about a user. 
- 
-*What is that health thingy?*
- Come and see [HP System explained](https://t.me/gangoffriendschannel/5)
-"""
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)
 GET_BIO_HANDLER = DisableAbleCommandHandler("bio", about_bio)
