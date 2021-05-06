eeaf6a883795ddb4126004c4a2895a78406ba6e9
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-05-06 07:43:18 +0530

    Update gettime.py

--- DaisyX/modules/gettime.py
+++ DaisyX/modules/gettime.py
@@ -96,5 +96,12 @@ TIME_HANDLER = DisableAbleCommandHandler("time", gettime)
 
 dispatcher.add_handler(TIME_HANDLER)
 
+__help__ = """
+✪ `/time <query>`*:*  Gives information about a timezone.
+
+Available queries: Country Code/Country Name/Timezone Name
+• 🕐 [Timezones list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
+"""
+__mod_name__ = "Time ⏱"
 __command_list__ = ["time"]
 __handlers__ = [TIME_HANDLER]
