68468a9f9ef900cd10cedfebcf14d81b5284bbc6
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-05-07 10:04:16 +0530

    Update misc.py

--- DaisyX/modules/misc.py
+++ DaisyX/modules/misc.py
@@ -624,10 +624,7 @@ __help__ = """
 *➩Search:*
   ✪ /google `<text>`:- search google queries.Use in bot pm (admin can use in group).
   ✪ /wiki: Search wikipedia articles.
-  ✪ /ud `<query>`: Search stuffs in urban dictionary.
-  ✪ /reverse: Reverse searches image or stickers on google.
   ✪ /app `<app name>`: Finds an app in playstore for you
-  ✪ /cash: currency converter
   ✪ /wall `<query>`: Get random wallpapers directly from bot!
   
 *➩Paste:*
