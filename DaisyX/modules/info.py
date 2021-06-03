01e80c5b2fbba260221c85f960e6b4e64f87c70c
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-03 12:08:21 +0530

    Update info.py

--- DaisyX/modules/info.py
+++ DaisyX/modules/info.py
@@ -557,3 +557,27 @@ __handlers__ = [
     GET_ABOUT_HANDLER,
     STATS_HANDLER,
 ]
+
+__help__ = """
+*ID:*
+ • /id*:* get the current group id. If used by replying to a message, gets that user's id.
+ • /gifid*:* reply to a gif to me to tell you its file ID.
+*Self addded information:* 
+ • /setme `<text>`*:* will set your info
+ • /me*:* will get your or another user's info.
+Examples:
+ `/setme I am a @DeshadeethThisarana`
+ `/me @username(defaults to yours if no user specified)`
+*Information others add on you:* 
+ • /bio*:* will get your or another user's bio. This cannot be set by yourself.
+• /setbio `<text>`*:* while replying, will save another user's bio 
+Examples:
+ `/bio @username(defaults to yours if not specified).`
+ `/setbio This user is a wolf` (reply to the user)
+*Overall Information about you:*
+ • /inf`*:* get information about a user. 
+ 
+*What is that health thingy?*
+ Come and see [HP System explained](https://t.me/gangoffriendschannel/5)
+"""
+__mod_name__ = "Info 🔬"
