2cb425318b9965ee69efb68d75ef72915d0a25f0
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-08 13:51:11 +0530

    Shadow

--- DaisyX/modules/animequotes.py
+++ DaisyX/modules/animequotes.py
@@ -13,7 +13,6 @@ from DaisyX.modules.disable import DisableAbleCommandHandler
 def animequotes(update: Update, context: CallbackContext):
     update.effective_message.reply_text(random.choice(animequotesstring.ANIMEQUOTES))
 
-
 ANIMEQUOTES_HANDLER = DisableAbleCommandHandler("animequotes", animequotes)
 
 dispatcher.add_handler(ANIMEQUOTES_HANDLER)
