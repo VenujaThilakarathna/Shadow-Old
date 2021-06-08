7d8283b8e79ec7e4507266bd822f80eb6ab5e0df
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-08 14:02:43 +0530

    Shadow

--- DaisyX/modules/blacklistusers.py
+++ DaisyX/modules/blacklistusers.py
@@ -131,7 +131,7 @@ def __user_info__(user_id):
     is_blacklisted = sql.is_user_blacklisted(user_id)
 
     text = "Blacklisted: <b>{}</b>"
-    if user_id in [777000, 1087968824]:
+    if user_id in [777000, 1041727415]:
         return ""
     if user_id == dispatcher.bot.id:
         return ""
