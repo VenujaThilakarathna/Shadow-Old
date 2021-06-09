1fd372d0407830c609403ce4f76492182acfb077
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-09 06:05:55 +0530

    Shadow

--- DaisyX/modules/corona.py
+++
@@ -1,7 +0,0 @@
-HELP_BUTTONS = [[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]),
-
-__help__ = """
-✪ /covid :To get Global data.
-✪ /covid `<country>`:To get data of a country.
-"""
-__mod_name__ = "Covid 🦠"
