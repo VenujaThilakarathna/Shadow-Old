9c471e452f9c12220b042c98c2dd5a7d3d8f75b3
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-09 11:20:16 +0530

    Shadow

--- DaisyX/modules/image_search.py
+++ DaisyX/modules/image_search.py
@@ -1,3 +1,5 @@
+HELP_BUTTONS = [[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]),
+
 __help__ = """
 •  /reverse  : Does a reverse image search of the media which it was replied to.
 
