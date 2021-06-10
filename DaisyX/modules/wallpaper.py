8b487faa14f08d30fee03609346428ce46683550
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-10 10:03:54 +0530

    Shadow

--- DaisyX/modules/wallpaper.py
+++ DaisyX/modules/wallpaper.py
@@ -55,6 +55,5 @@ def wall(update: Update, context: CallbackContext):
                     timeout=60,
                 )
 
-
 WALLPAPER_HANDLER = DisableAbleCommandHandler("wall", wall)
 dispatcher.add_handler(WALLPAPER_HANDLER)
