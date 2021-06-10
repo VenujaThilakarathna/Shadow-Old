23f161985cb6cb040b89fe9ee7c52580c01d9f52
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-10 12:51:44 +0530

    Shadow

--- DaisyX/modules/MoreTools.py
+++ DaisyX/modules/MoreTools.py
@@ -1,4 +1,4 @@
-HELP_BUTTONS = [[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]),
+HELP_BUTTONS = [[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]
 
 __help__ = """
 ** Blue Cleaner **
