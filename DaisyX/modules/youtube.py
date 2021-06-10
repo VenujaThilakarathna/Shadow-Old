b86ed94cb3802ecdfeb050ab1326048c95846a6f
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-10 11:02:20 +0530

    Shadow

--- DaisyX/modules/youtube.py
+++ DaisyX/modules/youtube.py
@@ -1,3 +1,5 @@
+HELP_BUTTONS = [[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]), 
+
 __help__ = """
  - /yt `<text>`: perform a youtube search
  - /ytaudio `<link>` or /ytvideo `<link>`: Downlods a video or audio from a youtube video to the bots local server
