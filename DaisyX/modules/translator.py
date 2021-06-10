366f48edaa314b1094c0afc9995320a0b776ec27
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-10 09:28:04 +0530

    Shadow

--- DaisyX/modules/translator.py
+++ DaisyX/modules/translator.py
@@ -1,3 +1,5 @@
+HELP_BUTTONS = [[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]),
+
 __help__ = """
  ✪ /tr or /tl: To translate to your language, by default language is set to english, use /tr <lang code> for some other language!
  ✪ /splcheck: As a reply to get grammar corrected text of gibberish message.
