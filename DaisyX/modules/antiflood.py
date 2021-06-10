f7b2506941edf638afdf77d60de80f708c8f6a72
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-10 12:54:44 +0530

    Shadow

--- DaisyX/modules/antiflood.py
+++ DaisyX/modules/antiflood.py
@@ -392,7 +392,7 @@ def __chat_settings__(chat_id, user_id):
     else:
         return "Antiflood has been set to`{}`.".format(limit)
 
-HELP_BUTTONS = [[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]),
+HELP_BUTTONS = [[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]
 
 __help__ = """
 You know how sometimes, people join, send 100 messages, and ruin your chat? With antiflood, that happens no more!
