54d08a0042f39b66ca93be05c45a3a5ea97b592e
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-10 11:04:28 +0530

    Shadow

--- DaisyX/modules/youtube.py
+++ DaisyX/modules/youtube.py
@@ -128,3 +128,11 @@ async def download_video(v_url):
             caption=ytdl_data["title"],
         )
         os.remove(f"{ytdl_data['id']}.mp4")
+
+HELP_BUTTONS = [[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]), 
+
+__help__ = """
+ - /yt `<text>`: perform a youtube search
+ - /ytaudio `<link>` or /ytvideo `<link>`: Downlods a video or audio from a youtube video to the bots local server
+"""
+__mod_name__ = "Youtube 🎞"
