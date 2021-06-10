409558d9f45cf9c3cf54610b130c1fff77fe3af1
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-10 07:04:39 +0530

    Shadow

--- DaisyX/modules/zombie.py
+++ DaisyX/modules/zombie.py
@@ -110,4 +110,10 @@ async def zombies(event):
 
     await cleaning_zombies.edit(del_status)
     
-  HELP_BUTTONS = [[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]), 
+HELP_BUTTONS = [[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]), 
+
+__help__ = """
+    ⦁ `/zombies`*:* Check if any deleted accounts available in the group member list and give results
+    ⦁ `/zombies` clean *:* Remove deleted accounts in the group member list 
+    """
+__mod_name__ = "Zombies ⚰️"
