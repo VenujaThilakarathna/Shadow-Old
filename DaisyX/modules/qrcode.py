6f277a7e7d820dbca03a448170db834c666bf44a
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-11 06:06:36 +0530

    Shadow

--- DaisyX/modules/qrcode.py
+++ DaisyX/modules/qrcode.py
@@ -1,4 +1,4 @@
-HELP_BUTTONS = [[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]),
+HELP_BUTTONS = [[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]
 
 __help__ = """
  ✪/getqr `<reply to image>`: Read QR code
