c50230a55e01293227b96cd9d61ccb60b6c581a8
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-14 05:23:21 +0530

    Shadow

--- DaisyX/modules/approval.py
+++ DaisyX/modules/approval.py
@@ -200,22 +200,6 @@ def unapproveall(update, context):
         "Successully unapproved all users from {}.".format(chat.title)
     )
         
-__mod_name__ = "Approval"    
-
-__help__ = """ 
-Sometimes, you might trust a user not to send unwanted content.
-Maybe not enough to make them admin, but you might be ok with auto warns, blacklists, and antiflood not applying to them.
-That's what approvals are for - approve of trustworthy users to allow them to send 
-Admin commands:
-✪ /approval*:* Check a user's approval status in this chat.
-Admin commands:
-✪ /approve*:* Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
-✪ /unapprove*:* Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
-✪ /approved*:* List all approved users.
-✪ /unapproveall*:* Unapprove ALL users in a chat. This cannot be undone.
-\
-"""    
-HELP_BUTTONS = [[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]
 
 APPROVED_HANDLER = DisableAbleCommandHandler("approved", approved, filters=Filters.group)
 UNAPPROVE_ALL_HANDLER = DisableAbleCommandHandler("unapproveall", unapproveall, filters=Filters.group)
