ef81f41b34fd3e6cb2104ef88cf66df562a60525
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-14 03:01:47 -0700

    Shadow

--- DaisyX/modules/youtube.py
+++ DaisyX/modules/youtube.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telethon.tl.types import DocumentAttributeAudio
 from youtube_dl import YoutubeDL
 from youtube_dl.utils import (
