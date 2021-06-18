108d04642bf7317efc49428142d95d92efbdb6c3
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-06-18 12:37:57 +0530

    Shadow

--- DaisyX/modules/log_channel.py
+++ DaisyX/modules/log_channel.py
@@ -193,14 +193,6 @@ if is_module_loaded(FILENAME):
             return f"This group has all it's logs sent to: {escape_markdown(log_channel_info.title)} (`{log_channel}`)"
         return "No log channel is set for this group!"
     
-    else:
-    # run anyway if module not loaded
-    def loggable(func):
-        return func
-
-    def gloggable(func):
-        return func
-
 HELP_BUTTONS = [[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]
     
 __help__ = """
