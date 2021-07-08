0af3dc7e48ccbf7d4ff7f21fa75fa6f6f0cbaf5e
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-07-08 15:45:39 +0530

    Shadow

--- DaisyX/modules/weather.py
+++
@@ -1,8 +0,0 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
-HELP_BUTTONS = [[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]
-
-__help__ = """
-✪ /weather `<city>`: Gets weather information of particular place!
-"""
-__mod_name__ = "Weather 🌧"
