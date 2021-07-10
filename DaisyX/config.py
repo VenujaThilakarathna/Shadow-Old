4f82ee1d3d6d8817bf33b2a2329f9979ac1dfa2f
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-07-10 18:00:00 +0530

    Shadow

--- DaisyX/config.py
+++ DaisyX/config.py
@@ -14,20 +14,20 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
+    API_ID =   # integer value, dont use ""
+    API_HASH = ""
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "Sawada"
     SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
-    JOIN_LOGGER = (-1001253661229)  # Prints any new group the bot is added to, prints just the name and ID.
-    EVENT_LOGS = (-1001190806654)  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    JOIN_LOGGER = (-1001593482680)  # Prints any new group the bot is added to, prints just the name and ID.
+    EVENT_LOGS = (-1001385654683)  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
     REDIS_URI = ""
     LOAD = []
-    NO_LOAD = ["rss", "cleaner", "connection"]
+    NO_LOAD = ["rss", "cleaner", "connection", "music_player", "info"]
     WEBHOOK = False
     INFOPIC = True
     URL = None
