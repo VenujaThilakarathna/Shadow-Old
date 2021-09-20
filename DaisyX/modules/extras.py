802e9dd9477d532208eb5c587856879b0fa1cecd
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-09-20 15:45:55 +0530

    Create extras.py

---
+++ DaisyX/modules/extras.py
@@ -0,0 +1,94 @@
+__help__ = """
+⚪️Groups Music Play⚪️ 
+1) Make bot admin with full permissions 
+2) Start a voice chat
+3) Try `/play song name` for the first time by an admin
+4) If userbot joined enjoy music, If not add @Shadow_Helper to your group and retry
+• `/play`: Play song using youtube music
+• `/play yt url`: Play the given yt url
+• `/play reply yo audio`: Play replied audio
+• `/dplay`: Play song via deezer
+• `/splay`: Play song via jio saavn
+• `/player`: Open Settings menu of player
+• `/skip`: Skips the current track
+• `/pause`: Pause track
+• `/resume`: Resumes the paused track
+• `/end`: Stops media playback
+• `/current`: Shows the current Playing track
+• `/playlist`: Shows playlist
+All other cmds except `/play`, `/current`, `/player` and `/playlist` are only for admins of the group. 
+🔴Channel Music Play🔴 
+1) Make *@Mr_Shadow_Robot* admin of your channel 
+2) Send `/userbotjoinchannel` in linked group
+3) Now send commands in linked group 
+• `/cplay song name` - play song you requested
+• `/cdplay song name` - play song you requested via deezer
+• `/csplay song name` - play song you requested via jio saavn
+• `/cplaylist` - Show now playing list
+• `/cccurrent` - Show now playing
+• `/cplayer` - open music player settings panel
+• `/cpause` - pause song play
+• `/cresume` - resume song play
+• `/cskip` - play next song
+• `/cend` - stop music play
+• `/userbotjoinchannel` - invite assistant to your chat
+Channel is also can be used instead of `c` ( /cplay = /channelplay )
+  
+🔰 If you don't like to play in linked group  
+1) Get your channel ID.
+2) Create a group with tittle: Channel Music: yourchannelid
+3) Add bot as Channel admin with full perms
+4) Add *@Mr_Shadow_Robot* to the channel as an admin.
+5) Simply send commands in your group.
+ 
+ ⚫️More tools⚫️ 
+• `/admincache`: Updates admin info of your group. Try if bot isn't recognize admin
+• `/userbotjoin`: Invite *@Shadow_Helper* Userbot to your chat 
+⚪️Groups Music Play⚪️ 
+1) Make bot admin with full permissions 
+2) Start a voice chat
+3) Try `/play song name` for the first time by an admin
+4) If userbot joined enjoy music, If not add @Shadow_Helper to your group and retry
+• `/play`: Play song using youtube music
+• `/play yt url`: Play the given yt url
+• `/play reply yo audio`: Play replied audio
+• `/dplay`: Play song via deezer
+• `/splay`: Play song via jio saavn
+• `/player`: Open Settings menu of player
+• `/skip`: Skips the current track
+• `/pause`: Pause track
+• `/resume`: Resumes the paused track
+• `/end`: Stops media playback
+• `/current`: Shows the current Playing track
+• `/playlist`: Shows playlist
+All other cmds except `/play`, `/current`, `/player` and `/playlist` are only for admins of the group. 
+🔴Channel Music Play🔴 
+1) Make *@Mr_Shadow_Robot* admin of your channel 
+2) Send `/userbotjoinchannel` in linked group
+3) Now send commands in linked group 
+• `/cplay song name` - play song you requested
+• `/cdplay song name` - play song you requested via deezer
+• `/csplay song name` - play song you requested via jio saavn
+• `/cplaylist` - Show now playing list
+• `/cccurrent` - Show now playing
+• `/cplayer` - open music player settings panel
+• `/cpause` - pause song play
+• `/cresume` - resume song play
+• `/cskip` - play next song
+• `/cend` - stop music play
+• `/userbotjoinchannel` - invite assistant to your chat
+Channel is also can be used instead of `c` ( /cplay = /channelplay )
+  
+🔰 If you don't like to play in linked group  
+1) Get your channel ID.
+2) Create a group with tittle: Channel Music: yourchannelid
+3) Add bot as Channel admin with full perms
+4) Add *@Mr_Shadow_Robot* to the channel as an admin.
+5) Simply send commands in your group.
+ 
+ ⚫️More tools⚫️ 
+• `/admincache`: Updates admin info of your group. Try if bot isn't recognize admin
+• `/userbotjoin`: Invite *@Shadow_Helper* Userbot to your chat 
+"""
+
+__mod_name__ = "🎴 Extras"
